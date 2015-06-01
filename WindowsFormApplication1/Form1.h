c1a948710725e90288001aa7c7d6b4b3341b0b38
Werkan <stawka@vp.pl>
2015-06-01 23:47:39 +0200

    przesyłanie nazw do drugiego okna

--- WindowsFormApplication1/Form1.h
+++ WindowsFormApplication1/Form1.h
@@ -43,42 +43,23 @@ namespace WindowsFormApplication1 {
 		}
 	private: System::Windows::Forms::Button^  button1;
 	protected:
-	protected: System::Windows::Forms::TextBox^  textBox1;
-	protected: System::Windows::Forms::Label^  label1;
-	protected: System::Windows::Forms::TextBox^  textBox2;
-	protected: System::Windows::Forms::TextBox^  textBox3;
-	protected: System::Windows::Forms::TextBox^  textBox4;
-	protected: System::Windows::Forms::TextBox^  textBox5;
-	protected: System::Windows::Forms::TextBox^  textBox6;
-	protected: System::Windows::Forms::TextBox^  textBox7;
-	protected: System::Windows::Forms::TextBox^  textBox8;
-	protected: System::Windows::Forms::TextBox^  textBox9;
-	protected: System::Windows::Forms::TextBox^  textBox10;
-	protected: System::Windows::Forms::TextBox^  textBox11;
-	protected: System::Windows::Forms::TextBox^  textBox12;
-	protected: System::Windows::Forms::TextBox^  textBox13;
-	protected: System::Windows::Forms::TextBox^  textBox14;
-	protected: System::Windows::Forms::TextBox^  textBox15;
-	protected: System::Windows::Forms::TextBox^  textBox16;
-	protected: System::Windows::Forms::TextBox^  textBox17;
-	protected: System::Windows::Forms::TextBox^  textBox18;
-	protected: System::Windows::Forms::TextBox^  textBox19;
-	protected: System::Windows::Forms::TextBox^  textBox20;
-	protected: System::Windows::Forms::TextBox^  textBox21;
-	protected: System::Windows::Forms::TextBox^  textBox22;
-	protected: System::Windows::Forms::TextBox^  textBox23;
-	protected: System::Windows::Forms::TextBox^  textBox24;
-	protected: System::Windows::Forms::TextBox^  textBox25;
-	protected: System::Windows::Forms::TextBox^  textBox26;
-	protected: System::Windows::Forms::TextBox^  textBox27;
-	protected: System::Windows::Forms::TextBox^  textBox28;
-	protected: System::Windows::Forms::TextBox^  textBox29;
-	protected: System::Windows::Forms::TextBox^  textBox30;
-	protected: System::Windows::Forms::TextBox^  textBox31;
-	protected: System::Windows::Forms::TextBox^  textBox32;
-	protected: System::Windows::Forms::TextBox^  textBox33;
-	protected: System::Windows::Forms::TextBox^  textBox34;
-	protected: System::Windows::Forms::TextBox^  textBox35;
+	public: System::Windows::Forms::TextBox^  textBox1;
+	public: System::Windows::Forms::Label^  label1;
+	public: System::Windows::Forms::TextBox^  textBox2;
+	public: System::Windows::Forms::TextBox^  textBox3;
+	public: System::Windows::Forms::TextBox^  textBox4;
+	public: System::Windows::Forms::TextBox^  textBox5;
+	public: System::Windows::Forms::TextBox^  textBox6;
+	public: System::Windows::Forms::TextBox^  textBox7;
+	public: System::Windows::Forms::TextBox^  textBox8;
+	public: System::Windows::Forms::TextBox^  textBox9;
+	public: System::Windows::Forms::TextBox^  textBox10;
+
+
+
+
+
+
 
 
 	private:
@@ -106,36 +87,11 @@ namespace WindowsFormApplication1 {
 			this->textBox8 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox9 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox10 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox11 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox12 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox13 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox14 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox15 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox16 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox17 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox18 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox19 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox20 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox21 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox22 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox23 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox24 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox25 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox26 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox27 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox28 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox29 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox30 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox31 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox32 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox33 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox34 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox35 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(178, 38);
+			this->button1->Location = System::Drawing::Point(12, 12);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(101, 34);
 			this->button1->TabIndex = 0;
@@ -145,7 +101,7 @@ namespace WindowsFormApplication1 {
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(318, 38);
+			this->textBox1->Location = System::Drawing::Point(135, 41);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(100, 20);
 			this->textBox1->TabIndex = 1;
@@ -153,7 +109,7 @@ namespace WindowsFormApplication1 {
 			// label1
 			// 
 			this->label1->AutoSize = true;
-			this->label1->Location = System::Drawing::Point(306, 22);
+			this->label1->Location = System::Drawing::Point(132, 23);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(121, 13);
 			this->label1->TabIndex = 2;
@@ -161,272 +117,72 @@ namespace WindowsFormApplication1 {
 			// 
 			// textBox2
 			// 
-			this->textBox2->Location = System::Drawing::Point(318, 64);
+			this->textBox2->Location = System::Drawing::Point(135, 67);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(100, 20);
 			this->textBox2->TabIndex = 3;
 			// 
 			// textBox3
 			// 
-			this->textBox3->Location = System::Drawing::Point(318, 246);
+			this->textBox3->Location = System::Drawing::Point(135, 249);
 			this->textBox3->Name = L"textBox3";
 			this->textBox3->Size = System::Drawing::Size(100, 20);
 			this->textBox3->TabIndex = 4;
 			// 
 			// textBox4
 			// 
-			this->textBox4->Location = System::Drawing::Point(318, 90);
+			this->textBox4->Location = System::Drawing::Point(135, 93);
 			this->textBox4->Name = L"textBox4";
 			this->textBox4->Size = System::Drawing::Size(100, 20);
 			this->textBox4->TabIndex = 5;
 			// 
 			// textBox5
 			// 
-			this->textBox5->Location = System::Drawing::Point(318, 116);
+			this->textBox5->Location = System::Drawing::Point(135, 119);
 			this->textBox5->Name = L"textBox5";
 			this->textBox5->Size = System::Drawing::Size(100, 20);
 			this->textBox5->TabIndex = 6;
 			// 
 			// textBox6
 			// 
-			this->textBox6->Location = System::Drawing::Point(318, 142);
+			this->textBox6->Location = System::Drawing::Point(135, 145);
 			this->textBox6->Name = L"textBox6";
 			this->textBox6->Size = System::Drawing::Size(100, 20);
 			this->textBox6->TabIndex = 7;
 			// 
 			// textBox7
 			// 
-			this->textBox7->Location = System::Drawing::Point(318, 168);
+			this->textBox7->Location = System::Drawing::Point(135, 171);
 			this->textBox7->Name = L"textBox7";
 			this->textBox7->Size = System::Drawing::Size(100, 20);
 			this->textBox7->TabIndex = 8;
 			// 
 			// textBox8
 			// 
-			this->textBox8->Location = System::Drawing::Point(318, 194);
+			this->textBox8->Location = System::Drawing::Point(135, 197);
 			this->textBox8->Name = L"textBox8";
 			this->textBox8->Size = System::Drawing::Size(100, 20);
 			this->textBox8->TabIndex = 9;
 			// 
 			// textBox9
 			// 
-			this->textBox9->Location = System::Drawing::Point(318, 220);
+			this->textBox9->Location = System::Drawing::Point(135, 223);
 			this->textBox9->Name = L"textBox9";
 			this->textBox9->Size = System::Drawing::Size(100, 20);
 			this->textBox9->TabIndex = 10;
 			// 
 			// textBox10
 			// 
-			this->textBox10->Location = System::Drawing::Point(318, 272);
+			this->textBox10->Location = System::Drawing::Point(135, 275);
 			this->textBox10->Name = L"textBox10";
 			this->textBox10->Size = System::Drawing::Size(100, 20);
 			this->textBox10->TabIndex = 11;
-			// 
-			// textBox11
-			// 
-			this->textBox11->Location = System::Drawing::Point(318, 298);
-			this->textBox11->Name = L"textBox11";
-			this->textBox11->Size = System::Drawing::Size(100, 20);
-			this->textBox11->TabIndex = 12;
-			// 
-			// textBox12
-			// 
-			this->textBox12->Location = System::Drawing::Point(318, 324);
-			this->textBox12->Name = L"textBox12";
-			this->textBox12->Size = System::Drawing::Size(100, 20);
-			this->textBox12->TabIndex = 13;
-			// 
-			// textBox13
-			// 
-			this->textBox13->Location = System::Drawing::Point(318, 350);
-			this->textBox13->Name = L"textBox13";
-			this->textBox13->Size = System::Drawing::Size(100, 20);
-			this->textBox13->TabIndex = 14;
-			// 
-			// textBox14
-			// 
-			this->textBox14->Location = System::Drawing::Point(318, 376);
-			this->textBox14->Name = L"textBox14";
-			this->textBox14->Size = System::Drawing::Size(100, 20);
-			this->textBox14->TabIndex = 15;
-			// 
-			// textBox15
-			// 
-			this->textBox15->Location = System::Drawing::Point(318, 402);
-			this->textBox15->Name = L"textBox15";
-			this->textBox15->Size = System::Drawing::Size(100, 20);
-			this->textBox15->TabIndex = 16;
-			// 
-			// textBox16
-			// 
-			this->textBox16->Location = System::Drawing::Point(318, 454);
-			this->textBox16->Name = L"textBox16";
-			this->textBox16->Size = System::Drawing::Size(100, 20);
-			this->textBox16->TabIndex = 17;
-			// 
-			// textBox17
-			// 
-			this->textBox17->Location = System::Drawing::Point(318, 428);
-			this->textBox17->Name = L"textBox17";
-			this->textBox17->Size = System::Drawing::Size(100, 20);
-			this->textBox17->TabIndex = 18;
-			// 
-			// textBox18
-			// 
-			this->textBox18->Location = System::Drawing::Point(318, 480);
-			this->textBox18->Name = L"textBox18";
-			this->textBox18->Size = System::Drawing::Size(100, 20);
-			this->textBox18->TabIndex = 19;
-			// 
-			// textBox19
-			// 
-			this->textBox19->Location = System::Drawing::Point(318, 740);
-			this->textBox19->Name = L"textBox19";
-			this->textBox19->Size = System::Drawing::Size(100, 20);
-			this->textBox19->TabIndex = 20;
-			// 
-			// textBox20
-			// 
-			this->textBox20->Location = System::Drawing::Point(318, 714);
-			this->textBox20->Name = L"textBox20";
-			this->textBox20->Size = System::Drawing::Size(100, 20);
-			this->textBox20->TabIndex = 21;
-			// 
-			// textBox21
-			// 
-			this->textBox21->Location = System::Drawing::Point(318, 688);
-			this->textBox21->Name = L"textBox21";
-			this->textBox21->Size = System::Drawing::Size(100, 20);
-			this->textBox21->TabIndex = 22;
-			// 
-			// textBox22
-			// 
-			this->textBox22->Location = System::Drawing::Point(318, 662);
-			this->textBox22->Name = L"textBox22";
-			this->textBox22->Size = System::Drawing::Size(100, 20);
-			this->textBox22->TabIndex = 23;
-			// 
-			// textBox23
-			// 
-			this->textBox23->Location = System::Drawing::Point(318, 636);
-			this->textBox23->Name = L"textBox23";
-			this->textBox23->Size = System::Drawing::Size(100, 20);
-			this->textBox23->TabIndex = 24;
-			// 
-			// textBox24
-			// 
-			this->textBox24->Location = System::Drawing::Point(318, 610);
-			this->textBox24->Name = L"textBox24";
-			this->textBox24->Size = System::Drawing::Size(100, 20);
-			this->textBox24->TabIndex = 25;
-			// 
-			// textBox25
-			// 
-			this->textBox25->Location = System::Drawing::Point(318, 584);
-			this->textBox25->Name = L"textBox25";
-			this->textBox25->Size = System::Drawing::Size(100, 20);
-			this->textBox25->TabIndex = 26;
-			// 
-			// textBox26
-			// 
-			this->textBox26->Location = System::Drawing::Point(318, 558);
-			this->textBox26->Name = L"textBox26";
-			this->textBox26->Size = System::Drawing::Size(100, 20);
-			this->textBox26->TabIndex = 27;
-			// 
-			// textBox27
-			// 
-			this->textBox27->Location = System::Drawing::Point(318, 532);
-			this->textBox27->Name = L"textBox27";
-			this->textBox27->Size = System::Drawing::Size(100, 20);
-			this->textBox27->TabIndex = 28;
-			// 
-			// textBox28
-			// 
-			this->textBox28->Location = System::Drawing::Point(318, 506);
-			this->textBox28->Name = L"textBox28";
-			this->textBox28->Size = System::Drawing::Size(100, 20);
-			this->textBox28->TabIndex = 29;
-			// 
-			// textBox29
-			// 
-			this->textBox29->Location = System::Drawing::Point(318, 922);
-			this->textBox29->Name = L"textBox29";
-			this->textBox29->Size = System::Drawing::Size(100, 20);
-			this->textBox29->TabIndex = 30;
-			// 
-			// textBox30
-			// 
-			this->textBox30->Location = System::Drawing::Point(318, 896);
-			this->textBox30->Name = L"textBox30";
-			this->textBox30->Size = System::Drawing::Size(100, 20);
-			this->textBox30->TabIndex = 31;
-			// 
-			// textBox31
-			// 
-			this->textBox31->Location = System::Drawing::Point(318, 870);
-			this->textBox31->Name = L"textBox31";
-			this->textBox31->Size = System::Drawing::Size(100, 20);
-			this->textBox31->TabIndex = 32;
-			// 
-			// textBox32
-			// 
-			this->textBox32->Location = System::Drawing::Point(318, 844);
-			this->textBox32->Name = L"textBox32";
-			this->textBox32->Size = System::Drawing::Size(100, 20);
-			this->textBox32->TabIndex = 33;
-			// 
-			// textBox33
-			// 
-			this->textBox33->Location = System::Drawing::Point(318, 818);
-			this->textBox33->Name = L"textBox33";
-			this->textBox33->Size = System::Drawing::Size(100, 20);
-			this->textBox33->TabIndex = 34;
-			// 
-			// textBox34
-			// 
-			this->textBox34->Location = System::Drawing::Point(318, 792);
-			this->textBox34->Name = L"textBox34";
-			this->textBox34->Size = System::Drawing::Size(100, 20);
-			this->textBox34->TabIndex = 35;
-			// 
-			// textBox35
-			// 
-			this->textBox35->Location = System::Drawing::Point(318, 766);
-			this->textBox35->Name = L"textBox35";
-			this->textBox35->Size = System::Drawing::Size(100, 20);
-			this->textBox35->TabIndex = 36;
 			// 
 			// Form1
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(552, 1015);
-			this->Controls->Add(this->textBox35);
-			this->Controls->Add(this->textBox34);
-			this->Controls->Add(this->textBox33);
-			this->Controls->Add(this->textBox32);
-			this->Controls->Add(this->textBox31);
-			this->Controls->Add(this->textBox30);
-			this->Controls->Add(this->textBox29);
-			this->Controls->Add(this->textBox28);
-			this->Controls->Add(this->textBox27);
-			this->Controls->Add(this->textBox26);
-			this->Controls->Add(this->textBox25);
-			this->Controls->Add(this->textBox24);
-			this->Controls->Add(this->textBox23);
-			this->Controls->Add(this->textBox22);
-			this->Controls->Add(this->textBox21);
-			this->Controls->Add(this->textBox20);
-			this->Controls->Add(this->textBox19);
-			this->Controls->Add(this->textBox18);
-			this->Controls->Add(this->textBox17);
-			this->Controls->Add(this->textBox16);
-			this->Controls->Add(this->textBox15);
-			this->Controls->Add(this->textBox14);
-			this->Controls->Add(this->textBox13);
-			this->Controls->Add(this->textBox12);
-			this->Controls->Add(this->textBox11);
+			this->ClientSize = System::Drawing::Size(294, 325);
 			this->Controls->Add(this->textBox10);
 			this->Controls->Add(this->textBox9);
 			this->Controls->Add(this->textBox8);
@@ -451,7 +207,14 @@ namespace WindowsFormApplication1 {
 
 	public: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
 				 Form2^ form2 = gcnew Form2;
-			
+				 form2->label1->Text = this->textBox1->Text;
+				 form2->label2->Text = this->textBox1->Text;
+				 form2->label3->Text = this->textBox1->Text;
+				 form2->label4->Text = this->textBox1->Text;
+				 form2->label5->Text = this->textBox1->Text;
+				 form2->label6->Text = this->textBox1->Text;
+				 form2->label7->Text = this->textBox1->Text;
+				 form2->label8->Text = this->textBox1->Text;
 				 Form2^ lista = gcnew Form2();
 				 lista->Show();
 
